0ee4da78636e6e1ebc6ef09c264c0b58d37cfff9
Alessia <Alessia@Mac_di_Alessia>
2022-05-08 12:26:17 +0200

    secondo commit

---
+++ src/ioo.asm
@@ -0,0 +1,27 @@
+
+
+	public start    ;Make the entry point public
+	org $2000       ;Place the origin at $2000
+
+
+start:
+	move #0, d0
+	move #0, d1
+	move #1, d2
+	
+	move #5, d1
+	
+	for_start:
+	tst  d1
+	ble  for_end
+	add #1, d0
+	sub d2, d1
+	add #2, d2
+	
+	bra for_start
+	
+		
+	for_end:
+	
+
+    end 
