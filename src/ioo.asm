8617fdb1d6b7e71c1e46306823bccf2fa138b12b
Alessia <Alessia@Mac_di_Alessia>
2022-05-08 13:00:47 +0200

    aggiorna

--- src/ioo.asm
+++ src/ioo.asm
@@ -6,19 +6,20 @@
 
 start:
 	move #0, d0
+	
 	move #0, d1
 	move #1, d2
 	
 	move #5, d1
 	
-	for_start:
+	for_begin:
 	tst  d1
 	ble  for_end
 	add #1, d0
 	sub d2, d1
 	add #2, d2
 	
-	bra for_start
+	bra for_begin
 	
 		
 	for_end:
